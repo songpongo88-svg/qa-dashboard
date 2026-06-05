8fb654864d9ac7ba252833b4be123bcba2d7d292
songpongo88-svg <songpongo88@gmail.com>
2026-06-05 18:26:49 +0700

    Point Firebase config to imported user project

--- src/firebaseClient.ts
+++ src/firebaseClient.ts
@@ -4,9 +4,9 @@ import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBn03smavKzc0l761okJQqCSyT0Wq022DQ",
-  authDomain: "qa-dashboard-bb5d.firebaseapp.com",
-  projectId: "qa-dashboard-bb5d",
-  storageBucket: "qa-dashboard-bb5d.firebasestorage.app",
+  authDomain: "qa-dashboard-b0b5d.firebaseapp.com",
+  projectId: "qa-dashboard-b0b5d",
+  storageBucket: "qa-dashboard-b0b5d.firebasestorage.app",
   messagingSenderId: "441715183213",
   appId: "1:441715183213:web:4e00da66b84546ff03964"
 };
@@ -14,3 +14,4 @@ const firebaseConfig = {
 export const firebaseApp = initializeApp(firebaseConfig);
 export const firebaseAuth = getAuth(firebaseApp);
 export const firebaseDb = getFirestore(firebaseApp);
+
