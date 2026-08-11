4afbb55a3726a3e8b4dd698ae0ef591e97917ef9
songpongo88-svg <songpongo88@gmail.com>
2026-08-11 17:21:49 +0700

    Lock monthly payment table to approved Preview v4 geometry

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { signaturePaymentPdfPatch } from './build/signaturePaymentPdfPatch.js'
+import { signaturePaymentPdfExactPreviewPatch } from './build/signaturePaymentPdfExactPreviewPatch.js'
 
 export default defineConfig({
-  plugins: [signaturePaymentPdfPatch(), react()],
+  plugins: [signaturePaymentPdfPatch(), signaturePaymentPdfExactPreviewPatch(), react()],
 })
