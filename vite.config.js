9a85fb4b97104900612c07afd9b81181b1afd9f2
songpongo88-svg <songpongo88@gmail.com>
2026-08-11 16:40:39 +0700

    Restore stable payment PDF build

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { signaturePaymentPdfPatch } from './build/signaturePaymentPdfPatch.js'
-import { signaturePaymentPdfLayoutPatch } from './build/signaturePaymentPdfLayoutPatch.js'
 
 export default defineConfig({
-  plugins: [signaturePaymentPdfPatch(), signaturePaymentPdfLayoutPatch(), react()],
+  plugins: [signaturePaymentPdfPatch(), react()],
 })
