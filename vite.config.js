f777beb7e9df6f8bfac4371fc4225a9cc79ae1ff
songpongo88-svg <songpongo88@gmail.com>
2026-08-14 13:08:20 +0700

    Apply larger monthly payment table fonts

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { signaturePaymentPdfPatch } from './build/signaturePaymentPdfPatch.js'
 import { signaturePaymentPdfExactPreviewPatch } from './build/signaturePaymentPdfExactPreviewPatch.js'
+import { signaturePaymentPdfTableFontBoostPatch } from './build/signaturePaymentPdfTableFontBoostPatch.js'
 import { sidebarGroupsDefaultOpenPatch } from './build/sidebarGroupsDefaultOpenPatch.js'
 
 export default defineConfig({
-  plugins: [signaturePaymentPdfPatch(), signaturePaymentPdfExactPreviewPatch(), sidebarGroupsDefaultOpenPatch(), react()],
+  plugins: [signaturePaymentPdfPatch(), signaturePaymentPdfExactPreviewPatch(), signaturePaymentPdfTableFontBoostPatch(), sidebarGroupsDefaultOpenPatch(), react()],
 })
