7bedeed36b6ab3aea80e92fc133b7b3864e122ac
songpongo88-svg <songpongo88@gmail.com>
2026-08-14 09:07:42 +0700

    Enable expanded sidebar groups by default

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { signaturePaymentPdfPatch } from './build/signaturePaymentPdfPatch.js'
 import { signaturePaymentPdfExactPreviewPatch } from './build/signaturePaymentPdfExactPreviewPatch.js'
+import { sidebarGroupsDefaultOpenPatch } from './build/sidebarGroupsDefaultOpenPatch.js'
 
 export default defineConfig({
-  plugins: [signaturePaymentPdfPatch(), signaturePaymentPdfExactPreviewPatch(), react()],
+  plugins: [signaturePaymentPdfPatch(), signaturePaymentPdfExactPreviewPatch(), sidebarGroupsDefaultOpenPatch(), react()],
 })
